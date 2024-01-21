8fac824254208963bda5424c0ff7d5cad01fc4b3
Bob Panganiban <czartmpanganiban@gmail.com>
2024-01-21 19:58:56 +0800

    aasd

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/authOptions";
 import Image from "next/image";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
   return (
     <main className="relative h-screen">
       <Image
